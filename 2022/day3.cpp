5aff92c364d73c8596214feece431b913ed6c9ea
Dylan Nelson <me@dylannelson.co.za>
2022-12-03 09:16:47 +0200

    fix cpp solution for day 3 part 1; was using strict inequality to test if character is lowercase

--- 2022/day3.cpp
+++ 2022/day3.cpp
@@ -19,14 +19,14 @@ int main() {
 
         for (int i = 0; i < rucksack.size()/2; ++i) {
             const char c = rucksack[i];
-            if (('a' < c) && (c < 'z')) priority = c - 'a' + 1;
+            if (('a' <= c) && (c <= 'z')) priority = c - 'a' + 1;
             else priority = c - 'A' + 27;
             left_compartment[priority - 1] = true;
         }
 
         for (int i = rucksack.size()/2; i < rucksack.size(); ++i) {
             const char c = rucksack[i];
-            if (('a' < c) && (c < 'z')) priority = c - 'a' + 1;
+            if (('a' <= c) && (c <= 'z')) priority = c - 'a' + 1;
             else priority = c - 'A' + 27;
             if (left_compartment[priority - 1]) {
                 if (!right_compartment[priority - 1]) total += priority;
