77da1a348cca8a8e7cdd7b0c78818fa570465fba
Dylan Nelson <me@dylannelson.co.za>
2023-12-05 19:26:33 +0100

    adapt solution for 2023 day 5 to handle second part of challenge

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,34 +1,64 @@
 #!/usr/bin/env python3
 
-seeds = list(input().split())[1:]
-seeds = list(map(int, seeds))
-
-input()
-
-
 def process_block(inputs):
     last_block = "location" in input()
     output_map = []
 
     line = input()
     while line:
-        output_map.append(list(map(int, line.split())))
+        destination_start, source_start, length = map(int, line.split())
+        output_map.append((
+            source_start,
+            source_start + length - 1,
+            destination_start,
+            destination_start + length - 1
+        ))
         line = input()
 
+    output_map.sort()
     output = []
-    for item in inputs:
-        for (output_start, input_start, length) in output_map:
-            if (input_start <= item) and (item < input_start + length):
-                output.append(output_start + item - input_start)
+
+    for (input_start, input_end) in inputs:
+        for (
+            source_start, source_end,
+            destination_start, destination_end
+        ) in output_map:
+            if source_start > input_end:
+                output.append((input_start, input_end))
                 break
+            if source_end < input_start:
+                continue
+            if input_start < source_start:
+                output.append((input_start, source_start - 1))
+                input_start = source_start
+            if input_end <= source_end:
+                output.append((
+                    destination_start + input_start - source_start,
+                    destination_start + input_end - source_start
+                ))
+                break
+            output.append((
+                destination_start + input_start - source_start,
+                destination_end
+            ))
+            input_start = source_end + 1
         else:
-            output.append(item)
+            output.append((input_start, input_end))
 
     return output, last_block
 
 
-location, last_block = process_block(seeds)
+seeds = input().split()[1:]
+seeds = list(map(int, seeds))
+
+seed_pairs = zip(seeds[::2], seeds[1::2])
+seed_pairs = [(start, start + length - 1) for (start, length) in seed_pairs]
+seed_pairs.sort()
+
+input()
+
+location, last_block = process_block(seed_pairs)
 while not last_block:
     location, last_block = process_block(location)
 
-print(min(location))
+print(min(location)[0])
