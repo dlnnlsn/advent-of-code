028d14c5a9fe7232d7995cbf347d22e93d25d34e
Dylan Nelson <me@dylannelson.co.za>
2023-12-09 15:10:47 +0100

    modify solution for 2023 day 9 to solve part 2

--- 2023/day9.hs
+++ 2023/day9.hs
@@ -8,6 +8,13 @@ predict list =
     else
         last list + predict (differences list)
 
+predict_backwards :: [Integer] -> Integer
+predict_backwards list =
+    if all (== 0) (differences list) then
+        head list
+    else
+        head list - predict_backwards (differences list)
+
 read_sequence :: String -> [Integer]
 read_sequence = map read . words
 
@@ -18,3 +25,5 @@ main = do
     let sequences = map read_sequence sequence_strings
     let predictions = map predict sequences
     putStrLn $ show $ sum predictions
+    let backwards_predictions = map predict_backwards sequences
+    putStrLn $ show $ sum backwards_predictions
